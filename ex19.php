8665dc78853f21fb0a0325a1350dbe4397c110d2
Filipe Rodrigues C. da Silva <filiperodrigues3003@gmail.com>
2018-09-25 16:14:00 -0300

    Status atual de LPW

--- ex19.php
+++ ex19.php
@@ -5,20 +5,20 @@
     $i = 0;
     $nome = $_POST["nome"];
     $sexo = $_POST["sexo"];
-    print_r($sexo);
+    
     while(isset($nome[$i])){
         if($sexo[$i]=="Masculino"){
             $homem++;
-            echo "$nome[$i] é homem <br>";
+            echo "$nome[$i] é <b>homem</b> <br>";
         }
         else{
             $mulher++;
-            echo "$nome[$i] é mulher <br>";
+            echo "$nome[$i] é <b>mulher</b> <br>";
         }
         $i++;
 
     }
 
-    echo "Foram informados $homem pessoas do sexo masculino e $mulher pessoas do sexo feminino";
+    echo "<br>Foram informados $homem pessoas do sexo masculino e $mulher pessoas do sexo feminino.";
     echo "<br><a href='javascript:history.go(-1)'>Voltar</a>";
 ?>
